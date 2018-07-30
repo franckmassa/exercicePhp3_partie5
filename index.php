a5af19bbe1c088b595060f0f21500ff4474d7ef9
massa <massafranck@gmail.com>
2018-07-30 15:00:15 +0200

    initial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice1</title>
+  </head>
+  <body>
+<p>
+<?php
+//Initialisation du tableau months
+$months [0] = 'janvier';
+$months [1] = 'février';
+$months [3] = 'mars';
+$months [4] = 'avril';
+$months [5] = 'mai';
+$months [6] = 'juin';
+$months [7] = 'juillet';
+$months [8] = 'août';
+$months [9] = 'sepetembre';
+$months [10] = 'octobre';
+$months [11] = 'novembre';
+$months [12] = 'décembre';
+//Affichage de l'index 5
+echo $months [5];
+ ?>
+</p>
+  </body>
+</html>
