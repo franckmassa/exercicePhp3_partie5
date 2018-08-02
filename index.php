a5afc09f7ef161a21d7867836cd47c3f7d3fecbc
massa <massafranck@gmail.com>
2018-08-02 23:03:41 +0200

    corrigé

--- index.php
+++ index.php
@@ -2,27 +2,15 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <title>Exercice3</title>
+    <title>Exercice 3/5</title>
   </head>
   <body>
-<p>
-<?php
-//Initialisation du tableau months
-$months [0] = 'janvier';
-$months [1] = 'février';
-$months [3] = 'mars';
-$months [4] = 'avril';
-$months [5] = 'mai';
-$months [6] = 'juin';
-$months [7] = 'juillet';
-$months [8] = 'août';
-$months [9] = 'sepetembre';
-$months [10] = 'octobre';
-$months [11] = 'novembre';
-$months [12] = 'décembre';
-//Affichage de l'index 5
-echo $months [5];
- ?>
-</p>
+    <p>
+      <?php
+        $months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre');
+        //Affichage de l'index 5 du tableau 
+        echo $months[5];
+      ?>
+    </p>
   </body>
 </html>
